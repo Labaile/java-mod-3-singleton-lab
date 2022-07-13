e81d866d67c1a983240d3c9af3b8f5dc85281655
Labaile <Labaile1@gmail.com>
2022-07-13 17:54:30 -0400

    implemented log method to print strings

---
+++ java-mod-3-singleton-lab/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import static java.rmi.server.LogStream.log;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getInstance();
+        logger.log("Hello World");
+        logger.log("Happy Wednesday!");
+
+    }
+}
